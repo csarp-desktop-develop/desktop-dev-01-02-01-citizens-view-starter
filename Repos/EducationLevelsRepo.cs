6b036dd692946abf8782c073e84471dbc3410b95
Gyuris Csaba <gycsaba72@gmail.com>
2024-02-26 12:30:06 +0100

    Initial commit

---
+++ Repos/EducationLevelsRepo.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Windows.Documents;
+
+namespace Kreta.Desktop.Repos
+{
+    public class EducationLevelsRepo
+    {
+        private List<String> _educationLevels= new() { "érettségi", "szakképzés", "szakmai vizsga" };
+        public List<string> FindAll()
+        {
+            return _educationLevels;
+        }
+
+        public string FindFirst()
+        {
+            return FindAll().FirstOrDefault() ?? string.Empty;
+        }
+    }
+}
---
+++ ViewModels/StudentViewModel.cs
@@ -0,0 +1,59 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+using CommunityToolkit.Mvvm.Input;
+using Kreta.Desktop.Repos;
+using MenuProject.ViewModels.Base;
+using StudentProject.Models;
+using System.Collections.ObjectModel;
+
+namespace MenuProject.ViewModels
+{
+    public partial class StudentViewModel : BaseViewModel
+    {
+        private readonly EducationLevelsRepo _educationLevelsRepo = new();
+        private readonly StudentRepo _studentRepo = new();
+
+        [ObservableProperty]
+        private ObservableCollection<string> _educationLevels = new();
+
+        [ObservableProperty]
+        private ObservableCollection<Student> _students = new();
+
+        [ObservableProperty]
+        private Student _selectedStudent;        
+        
+        public StudentViewModel()
+        {
+            _selectedStudent = new Student();
+            Update();
+        }
+
+        [RelayCommand]
+        public void DoSave(Student student)
+        {
+            if (student.HasId)
+                _studentRepo.Update(student);
+            else
+                _studentRepo.Insert(student);
+            Update();
+        }
+
+        [RelayCommand]
+        void DoNewStudent()
+        {
+            SelectedStudent = new Student();
+        }
+
+        [RelayCommand]
+        public void DoRemove(Student studentToDelete)
+        {
+            _studentRepo.Delete(studentToDelete);
+            Update();
+        }
+
+        private void Update()
+        {
+            EducationLevels = new ObservableCollection<string>(_educationLevelsRepo.FindAll());
+            Students = new ObservableCollection<Student>(_studentRepo.FindAll());
+        }
+    }
+}
